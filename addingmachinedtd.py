12ab658840fbba47fec9d2e84aa507e92d548f2f
FooFighterII <102966916+FooFighterII@users.noreply.github.com>
2022-04-05 07:47:29 -0400

    Update File

---
+++ addingmachinedtd.py
@@ -0,0 +1,5 @@
+print("This is an adding machine")
+first_number = int(input("What is the first number that you want to add?"))
+second_number = int(input("What is the second number that you want to add?"))
+sum = first_number + second_number
+print("The sum of your two numbers is", sum)
