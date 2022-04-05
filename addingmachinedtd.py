43bfc6accb18319d12fe61e555eeec024455b929
FooFighterII <102966916+FooFighterII@users.noreply.github.com>
2022-04-05 07:55:35 -0400

    Added checking, via except

---
+++ addingmachinedtd.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        print("This is an adding machine")
+        first_number = int(input("What is the first number that you want to add?: "))
+        second_number = int(input("What is the second number that you want to add?: "))
+        sum = first_number + second_number
+        print("The sum of your two numbers is", sum)
+        break
+    except:
+        print("Your input is not correct...")
